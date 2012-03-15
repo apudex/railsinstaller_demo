5bb253f764a20aa9315f48d212cb2ed6d33b5027
apud <apud@apud.com>
2012-03-15 13:08:37 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'c91bc1491d3bb0500d58ff2226381032dab088057ea6d1ebda4eca533d0de4fee8b536b042644efd14b041c50b4d8fe19c38824787a9f14f55520b63588367ca'
